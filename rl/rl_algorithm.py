91f67c55ce9c24b906867ecdb218b06015941930
Guillermo <guillermo.infante@upf.edu>
2023-06-09 16:07:56 +0200

    update

--- rl/rl_algorithm.py
+++ rl/rl_algorithm.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
 from typing import Union
 import numpy as np
-from torch.utils.tensorboard import SummaryWriter
+# from torch.utils.tensorboard import SummaryWriter
 import torch as th
 from gym.spaces import Discrete, Box
+
 
 class RLAlgorithm(ABC):
 
@@ -14,7 +15,8 @@ class RLAlgorithm(ABC):
             self.action_dim = self.env.action_space.n
         else:
             self.action_dim = self.env.action_space.shape[0]
-        self.device = th.device('cuda' if th.cuda.is_available() else 'cpu') if device == 'auto' else device
+        self.device = th.device('cuda' if th.cuda.is_available(
+        ) else 'cpu') if device == 'auto' else device
 
         self.num_timesteps = 0
         self.num_episodes = 0
@@ -29,12 +31,12 @@ class RLAlgorithm(ABC):
         Returns:
             np.array or int: Action
         """
-    
+
     @abstractmethod
     def train(self):
         """Update algorithm's parameters
         """
-    
+
     @abstractmethod
     def get_config(self) -> dict:
         """Generates dictionary of the algorithm parameters configuration
@@ -57,15 +59,15 @@ class RLAlgorithm(ABC):
     def setup_wandb(self, project_name, experiment_name):
         self.experiment_name = experiment_name
         import wandb
-        wandb.init(project=project_name, 
-                    sync_tensorboard=True, 
-                    config=self.get_config(), 
-                    name=self.experiment_name, 
-                    monitor_gym=True, 
-                    save_code=True)
-        self.writer = SummaryWriter(f"/tmp/{self.experiment_name}")
-    
+        wandb.init(project=project_name,
+                   sync_tensorboard=True,
+                   config=self.get_config(),
+                   name=self.experiment_name,
+                   monitor_gym=True,
+                   save_code=True)
+        # self.writer = SummaryWriter(f"/tmp/{self.experiment_name}")
+
     def close_wandb(self):
         import wandb
-        self.writer.close()
+        # self.writer.close()
         wandb.finish()
