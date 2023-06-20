0a2b645ee40ebe8e7f4599f16bd1f9d571106155
Guillermo <guillermo.infante@upf.edu>
2023-06-20 14:08:02 +0200

    Policy reader with the example of the 2 coffees and 2 office locations

--- policy_reader_coffee.py
+++ policy_reader_coffee.py
@@ -1,32 +1,73 @@
 import pickle as pkl
 import numpy as np
 import os
-import argparse
+# import argparse
 
 if __name__ == "__main__":
 
-    # This is to check how the SF representation of the discovered policies look like
-
     actions = ["LEFT", "UP", "RIGHT", "DOWN"]
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input", type=str, required=True)
+    # Enumerate the relevant locations (check the environment)
+    coffee1 = (2, 3, 0, 0)
+    coffee2 = (3, 6, 0, 0)
+    init_state = (5, 3, 0, 0)
 
-    args = parser.parse_args()
-    dir = args.input
+    # 1) Get the SF representstion for the policies in the CCS for the office task.
+    dirpath = os.path.abspath("policies/Office-v0")
 
-    dirpath = os.path.abspath(f"policies/{dir}")
+    SFs = []
 
     for i, file in enumerate(os.listdir(dirpath)):
         with open(os.path.join(dirpath, file), "rb") as fp:
             policy = pkl.load(fp)
-
-        print(f"\nPolicy {i}")
-        print(policy["reward"])
         q = policy["q_table"]
-        w = np.asarray([0.7737809366318175, 0.95])
-        qc1 = np.dot(q[(6, 6, 0, 0)], w)
-        # qc2 = np.dot(q[(3, 6, 0, 0)], w)
-        print('c1', np.max(qc1), actions[np.argmax(qc1)], qc1)
-        # print('c2', np.max(qc2), np.argmax(qc2))
-        print(15 * '--')
+        SFs.append(q)
+
+    # We set the weights to 1 for each of the offices as any of the sastisfies the task.
+    w = np.asarray([1, 1])
+
+    # We compute the q-function for each policy and for each 'coffee' location.
+    # Then we can apply GPI (see Theorem 1 in [1])
+
+    q_values_coffee1 = np.asarray([np.dot(q[coffee1], w) for q in SFs])
+    q_values_coffee2 = np.asarray([np.dot(q[coffee2], w) for q in SFs])
+
+    value_coffee1 = np.max(q_values_coffee1)
+    value_coffee2 = np.max(q_values_coffee2)
+    print(f'Value for coffee at {coffee1}', value_coffee1)
+    print(f'Value for coffee at {coffee2}', value_coffee2)
+
+    # Such values there after are the weights to combine the coffee subtask.
+    w = np.asarray([value_coffee1, value_coffee2])
+
+    # Now get the policies in the CCS in the coffee subtask
+    dirpath = os.path.abspath("policies/Coffee-v0")
+
+    SFs = []
+
+    for i, file in enumerate(os.listdir(dirpath)):
+        with open(os.path.join(dirpath, file), "rb") as fp:
+            policy = pkl.load(fp)
+        q = policy["q_table"]
+        SFs.append(q)
+
+    # We compute the q-function for each policy for the initial state.
+    # Then we can apply GPI (see Theorem 1 in [1])
+
+    q_values_init_state = np.asarray([np.dot(q[init_state], w) for q in SFs])
+
+    argmax = np.unravel_index(
+        np.argmax(q_values_init_state), q_values_init_state.shape)
+
+    value_init_state = np.max(q_values_init_state)
+    print(f'Value for init_state at {init_state}', value_init_state)
+    print("Optimal action at init_state after composing:", actions[argmax[-1]])
+
+    # The value for going to the RIGHT is 0.7737809040955714 which is exactly .95 ** (N-1) for N = 6 that is the length of the
+    # optimal path.
+
+    """
+    References
+    ----------
+    [1] Barreto, André et al. "Successor Features for Transfer in Reinforcement Learning".
+    """
