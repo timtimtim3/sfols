805bac8f8fd2d8f96d3d3869287074b5357f121a
Kuroo <dkuric20@gmail.com>
2023-08-29 17:23:29 +0200

    Restructure env, add transition probs

--- envs/teleport.py
+++ envs/teleport.py
@@ -63,6 +63,48 @@ class Teleport(gym.Env):
         # NOTE: The osbservation_space is (X, Y, <objects_collected>)
         self.observation_space = Box(low=np.zeros(
             2), high=np.ones(2))
+        self._create_coord_mapping()
+        self._create_transition_function()
+
+    def _create_coord_mapping(self):
+        """
+        Create mapping from coordinates to state id and inverse mapping
+        """
+        self.state_to_coords = {}
+        idx = 0
+        for i in range(0, self.MAP.shape[0]):
+            for j in range(0, self.MAP.shape[1]):
+                if self.MAP[i][j] == "X":
+                    continue
+                self.state_to_coords[idx] = (i, j)
+                idx += 1
+        self.coords_to_state = dict(reversed(item) for item in self.state_to_coords.items())
+
+    def _create_transition_function(self):
+        # Basic movement
+        s_dim = len(self.state_to_coords)
+        a_dim = self.action_space.n
+        self.P = np.zeros((s_dim, a_dim, s_dim))
+        for start_s in range(s_dim):
+            for a in range(a_dim):
+                start_coords = self.state_to_coords[start_s]
+                new_coords = self.base_movement(start_coords, a)
+                new_s = self.coords_to_state[new_coords]
+                self.P[start_s, a, new_s] = 1
+
+        # Specific additions
+        teleport_state = self.coords_to_state[(1, 5)]
+        for start_s in range(s_dim):
+            for a in range(a_dim):
+                if self.P[start_s, a, teleport_state] == 1:
+                    self.P[start_s, a, teleport_state] = 0
+                    for i in self.teleport_ends:
+                        self.P[start_s, a, self.coords_to_state[i]] = 1.0/len(self.object_ids)
+
+
+        # sanity check
+        assert np.allclose(np.sum(self.P, axis=2), 1)
+
 
     def state_to_array(self, state):
         return np.array(state, dtype=np.int32)
@@ -71,51 +113,42 @@ class Teleport(gym.Env):
         self.state = random.choice(self.initial)
         return self.state_to_array(self.state)
 
-    def step(self, action):
-        old_state = self.state
-        row, col = self.state
-
-        # move
-        if action == Teleport.LEFT:
+    def base_movement(self, coords, action):
+        row, col = coords
+        if action == self.LEFT:
             col -= 1
-        elif action == Teleport.UP:
+        elif action == self.UP:
             row -= 1
-        elif action == Teleport.RIGHT:
+        elif action == self.RIGHT:
             col += 1
-        elif action == Teleport.DOWN:
+        elif action == self.DOWN:
             row += 1
         else:
             raise Exception('bad action {}'.format(action))
+        if col < 0 or col >= self.width or row < 0 or row >= self.height or (row, col) in self.occupied: # no move
+            return coords
+        else:
+            return (row, col)
 
-        # out of bounds, cannot move
-        if col < 0 or col >= self.width or row < 0 or row >= self.height:
-            return self.state_to_array(self.state), 0., False, {'phi': np.zeros(len(self.all_objects), dtype=np.float32)}
 
-        # into a blocked cell, cannot move
-        s1 = (row, col)
-        if s1 in self.occupied:
-            return self.state_to_array(self.state), 0., False, {'phi': np.zeros(len(self.all_objects), dtype=np.float32)}
+    def step(self, action):
+        old_state = self.state
 
-        if s1 in self.teleport_start:
-            s1 = random.sample(self.teleport_ends, 1)[0]  # Teleport
-            self.state = s1
-            phi = self.features(old_state, action, self.state)
-            reward = np.dot(phi, self.w)
-            return self.state_to_array(self.state), reward, True, {'phi': phi}
-        # can now move
-        # NOTE: The collected part of the state plays a ~similar~ role to a reward machine.
-        self.state = s1
+        # Base movement
+        new_state = self.base_movement(self.state, action)
 
-        # into a shape cell
-        if s1 in self.object_ids:
+        # Teleport check
+        if new_state in self.teleport_start:
+            new_state = random.sample(self.teleport_ends, 1)[0]  # Teleport
+            self.state = new_state
 
-            self.state = s1
-            phi = self.features(old_state, action, self.state)
-            reward = np.dot(phi, self.w)
-            return self.state_to_array(self.state), reward, True, {'phi': phi}
+        self.state = new_state
 
-        # into an empty cell
-        return self.state_to_array(self.state), 0., False, {'phi': np.zeros(len(self.all_objects), dtype=np.float32)}
+        # Determine features and rewards
+        phi = self.features(old_state, action, new_state)
+        reward = np.dot(phi, self.w)
+        done = new_state in self.object_ids
+        return self.state_to_array(self.state), reward, done, {'phi': phi}
 
     # ===========================================================================
     # STATE ENCODING FOR DEEP LEARNING
