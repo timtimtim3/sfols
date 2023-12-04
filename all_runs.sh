c7fb6500f2e4a71fef491e6f902523a232b6ff10
Guillermo <guillermo.infante@upf.edu>
2023-12-04 11:06:06 +0100

    added bash script for runs

---
+++ all_runs.sh
@@ -0,0 +1,21 @@
+# python run.py +experiment=delivery-qlearning ++seed=1
+# python run.py +experiment=delivery-qlearning ++seed=10
+# python run.py +experiment=delivery-qlearning ++seed=13
+# python run.py +experiment=delivery-qlearning ++seed=78
+# python run.py +experiment=delivery-qlearning ++seed=84
+# python run.py +experiment=delivery-qlearning ++seed=1006
+# python run.py +experiment=delivery-qlearning ++seed=1007
+# python run.py +experiment=delivery-qlearning ++seed=1008
+# python run.py +experiment=delivery-qlearning ++seed=1009
+# python run.py +experiment=delivery-qlearning ++seed=1010
+
+python run.py +experiment=office-qlearning ++seed=1
+python run.py +experiment=office-qlearning ++seed=10
+python run.py +experiment=office-qlearning ++seed=13
+python run.py +experiment=office-qlearning ++seed=78
+python run.py +experiment=office-qlearning ++seed=84
+# python run.py +experiment=office-qlearning ++seed=1006
+# python run.py +experiment=office-qlearning ++seed=1007
+# python run.py +experiment=office-qlearning ++seed=1008
+# python run.py +experiment=office-qlearning ++seed=1009
+# python run.py +experiment=office-qlearning ++seed=1010
